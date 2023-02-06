20139ec41d6906202bb65f92610e101be380ae0e
diego-domajnko <diegodomajnko@gmail.com>
2023-02-06 16:15:35 -0300

    Responsivo mobile

--- js/main.js
+++ js/main.js
@@ -2,13 +2,10 @@ const hamburguer = document.querySelector('.fa-bars');
 
 hamburguer.addEventListener("click", () => {
     const menu = document.querySelector(".nav__lista").classList;
-    const section = document.querySelector(".corpo__sobre-nos");
 
     if (menu.contains("menu--ativo")) {
         menu.remove("menu--ativo");
-        section.style.marginTop = "";
     } else {
         menu.add("menu--ativo");
-        section.style.marginTop = "80px";
-    }
+    };
 });
